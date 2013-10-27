28d8f93acaace24966bc04b92f55093a5e9594ea
Anjian Wu <awwu@caltech.edu>
2013-10-27 12:29:47 -0700

    updated pseudo

--- Hwk 3/hw3_pseudo.asm
+++ Hwk 3/hw3_pseudo.asm
@@ -97,89 +97,19 @@ NAME        HW3
 ;-------------------------------------------------------------------------------
 
 
-Dec2String		PROC    NEAR
-				PUBLIC  Dec2String
-				
-DecSignCheck:
-
-		CMP     AX, 0                   ; Check to see if Num is neg
-        JGE     DecIsPos                ; Num is not neg, thus no '-' needed
-        ;JL                             ; Num is neg, need 2's complement and '-' 
-DecIsNeg:
-        NEG     AX                      ;
-        MOV     BL, '-'                 ;
-        MOV     DS:[SI], BL             ;Place the character in mem
-        INC     SI                      ;Update the char pointer
-       
-        JMP     Bin2BCDInit             ;
-DecIsPos:
-        MOV     BL, '0'                 ;
-        MOV     DS:[SI], BL             ;
-
-        INC     SI                      ;Update the char pointer
-		;JMP    Bin2BCDInit
-        
-Bin2BCDInit:                            ;initialization
-        MOV     BX, AX                  ;BX = arg. Save the arg
-
-        MOV     CX, 10000                ;start with 10^3 (1000's digit)
-        CLC                             ;no error yet
-        ;JMP    Bin2BCDLoop             ;now start looping to get digits
 
 
-Bin2BCDLoop:                            ;loop getting the digits in arg
-
-        JC      Dec2StringEND          ;if there is an error - we're done
-        CMP     CX, 0                   ;check if pwr10 > 0
-        JLE     Dec2StringEND          ;if not, have done all digits, done
-        ;JMP    Bin2BCDLoopBody         ;else get the next digit
-
-Bin2BCDLoopBody:                        ;get a digit
-        MOV     AX, BX
-        MOV     DX, 0                   ;setup for arg/pwr10 remainder
-
-        DIV     CX                      ;digit (AX) = arg/pwr10
-        CMP     AX, 10                  ;check if digit < 10
-        JAE     TooBigError             ;if not, it's an error
-        ;JB     HaveDigit               ;otherwise process the digit
-
-HaveDigit:                              ;put the digit into the result
-      
-        CALL    StoreDaChar             ;Store Char, BX is changed, but that's OK
-        INC     SI                      ;Update the char pointer
-        
-DecUpdatePWR10:
-
-        MOV     BX, DX                  ;now work with arg = arg MODULO pwr10
-        MOV     AX, CX                  ;setup to update pwr10
-        MOV     CX, 10                  ;   (clears CH too)
-        MOV     DX, 0                   ;setup for arg/pwr10 remainder   
-        DIV     CX                      ;   (note: pwr10/10 <= 100 so no overflow)
-        MOV     CX, AX                  ;pwr10 = pwr10/10 (note: CH = 0)
-        CLC                             ;no error
-        JMP     EndBin2BCDLoopBody      ;done getting this digit
-
-TooBigError:                            ;the value was too big
-        STC                             ;set the error flag
-        ;JMP    EndBin2BCDLoopBody      ;and done with this loop iteration
-
-EndBin2BCDLoopBody:
-        JMP     Bin2BCDLoop             ;keep looping (end check is at top)
-
-Dec2StringEND:
-        MOV     AX, 0          ;
-        MOV     DS:[SI], AL             ;Place the character in mem
-        
-        RET
-       
-Dec2String      ENDP    
-
-
-;Procedure:			StoreDaChar
+;Procedure:			QueueFull
 ;
-;Description:       This function returns the integer square root of the
-;                   8-bit unsigned integer passed to the function.  The
-;                   returned value is rounded to the nearest integer.
+;Description:       This function take the address of the queue at 'a' to
+;                   see if it is FULL. It does this by looking at the
+;                   head/tailed pointers and queue length of address 'a'
+;                   doing the following calculation.
+;                   (Tail + 1) % length = X. If X = H then it is FULL,
+;                   else it is not full.
+;
+;                   If it is full zeroflag -> true
+;                   If it is not full; zeroflag -> reset
 ;
 ;Operation:         The passed integer is looked up in a table containing the
 ;                   square root of every integer (a 256 entry table).
@@ -220,47 +150,56 @@ Dec2String      ENDP
 ;History:			Pseudo code - 10-21-2013
 ;-------------------------------------------------------------------------------
 
-StoreDaChar		PROC    NEAR
-				PUBLIC  StoreDaChar
-                
-                
-        MOV	    BX, OFFSET(ASCIICharTable);point into the table of square roots
-        XLAT	CS:ASCIICharTable		;get the square root   
-        
-        MOV     DS:[SI], AL             ;Place the character in mem
- ;       INC     SI                      ;Update the char pointer
-        
-        RET
-        
-StoreDaChar         ENDP
 
-; ASCIICharTable
+;Procedure:			Dequeue
 ;
-; Description:      This table contains the ASCII char of Hex 0 - F
+;Description:       This function take the address of the queue at 'a' to
+;                   see if it is FULL. It does this by looking at the
+;                   head/tailed pointers and queue length of address 'a'
+;                   doing the following calculation.
+;                   (Tail + 1) % length = X. If X = H then it is FULL,
+;                   else it is not full.
 ;
-; Author:           Anjian Wu
-; Last Modified:    10-23-2013
+;                   If it is full zeroflag -> true
+;                   If it is not full; zeroflag -> reset
+;
+;Operation:         The passed integer is looked up in a table containing the
+;                   square root of every integer (a 256 entry table).
+;
+; Arguments:         AL - unsigned value for which to compute the square root.
+; Return Value:      AL - integer square root of the passed argument.
+;
+;
+;Shared Variables: 	None.
+;
+;Local Variables:	Sign -> INT sign flag
+;					pwr10 -> INT current power of 10
+;					error ->  INT Error flag
+;					digit -> BCD each digit from algorithm
+;Global Variables:	None.
+;
+;
+;Input:            	None.
+;Output:           	String in (DS:SI). Fixed size of 5 bytes.
+;
+;Registers Changed: flags, BX.
+;
+;Stack Depth:		None.
+;
+;Known Bugs:		None for now.
+;
+;Data Structures:	None.
+;
+;Error Handling:   	The program has an error flag that is raised if the Dec2BCD
+;					runs into error. If so, the stored value is undefined.
+;
+;Algorithms:       	 Table lookup.
+;
+;Limitations:  		Only handles numbers less than 5 digits. Will always
+;					allocate 5 characters worth space in mem for return
+;					value (e.g. 1 -> +0001).
+;Author:			Anjian Wu
+;History:			Pseudo code - 10-21-2013
+;-------------------------------------------------------------------------------
 
 
-ASCIICharTable	LABEL	BYTE
-
-	DB	 '0'
-    DB	 '1'
-    DB	 '2'
-    DB	 '3'
-    DB	 '4'
-    DB	 '5'
-    DB	 '6'
-    DB	 '7'
-    DB	 '8'
-    DB	 '9'
-    DB	 'A'
-    DB	 'B'
-    DB	 'C'
-    DB	 'D'
-    DB	 'E'
-    DB	 'F'
-    
-CODE ENDS
-
-        END
